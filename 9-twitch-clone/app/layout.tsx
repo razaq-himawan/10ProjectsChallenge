abcb9e4ba8a8503fcc0c5cf9d52378717170f93e
RazaqHimawan <razaqhim@gmail.com>
2024-08-15 11:53:10 +0700

    Update clerk layout, dark theme

--- 9-twitch-clone/app/layout.tsx
+++ 9-twitch-clone/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 
 import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,9 +19,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl={'/'}>
+    <ClerkProvider
+      afterSignOutUrl={'/'}
+      appearance={{ baseTheme: dark }}
+    >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            forcedTheme="dark"
+            storageKey="twc-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
